abd0860ac73abbdfbda61df369ffb26c182ba63c
Edwin Blommaerts <edwin.blommaerts@gmail.com>
2023-04-20 16:35:08 +0100

    [FIX] attachment file name fields

--- muk_utils/models/ir_attachment.py
+++ muk_utils/models/ir_attachment.py
@@ -52,7 +52,7 @@ class IrAttachment(models.Model):
         vals.update({
             'file_size': len(bin_data),
             'checksum': self._compute_checksum(bin_data),
-            'index_content': self._index(bin_data, attach.datas_fname, attach.mimetype),
+            'index_content': self._index(bin_data, attach.store_fname, attach.mimetype),
         })
         return vals
     
